79003d64e817499208cc3b472ce254993b71b913
Samu-fabi <153144113+Samu-fabi@users.noreply.github.com>
2025-11-20 11:56:53 -0500

    feat: Implementación de Layout fijo, Carrusel Bootstrap y estructura Grid. Se corrige dependencia JS.

---
+++ Clase01/Controllers/NegociosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Clase01.Controllers
+{
+    public class NegociosController : Controller
+    {
+        // GET: Negocios
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
